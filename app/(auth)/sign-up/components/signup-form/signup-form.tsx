40acf62366865bcce7302a2e3b8f341cb40aacf1
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-04-26 15:34:45 +0300

    customize the input if it's type is checkbox.

--- app/(auth)/sign-up/components/signup-form/signup-form.tsx
+++ app/(auth)/sign-up/components/signup-form/signup-form.tsx
@@ -1,7 +1,7 @@
 "use client" 
 import MotionField from "@/components/motionTextField/motionTextField";
 import {Button} from "@/components/ui/button";
-import {Form, FormikProvider, useFormik} from "formik";
+import {Form, FormikProvider} from "formik";
 
 import useForm from "./hooks/useForm";
 import {Loader2} from "lucide-react";
@@ -14,11 +14,21 @@ const SignUpForm = () => {
             <Form>
                 <MotionField label="Full Name" name="name"
                     isPassword={false}
-                    placeholder="Moamen Yazouri"/>
+                    placeholder="Moamen Yazouri"
+                    style={{
+                        label: "",
+                        input: "w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500"
+                    }}
+                />
 
                 <MotionField label="Email" name="email"
                     isPassword={false}
-                    placeholder="email@example.com"/>
+                    placeholder="email@example.com"
+                    style={{
+                        label: "",
+                        input: "w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500"
+                    }}
+                />
 
                 <MotionedSelect label="Role" name="role"
                     options={
@@ -33,13 +43,27 @@ const SignUpForm = () => {
                         ]
                     }/>
 
-                <MotionField label="password" name="password"
+                <MotionField 
+                    label="password" 
+                    name="password"
                     isPassword={true}
-                    placeholder="••••••••"/>
+                    placeholder="••••••••"
+                    style={{
+                        label: "",
+                        input: "w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500" 
+                    }}
+                />
 
-                <MotionField label="Confirm Password" name="confirmedPassword"
+                <MotionField 
+                    label="Confirm Password" 
+                    name="confirmedPassword"
                     isPassword={true}
-                    placeholder="••••••••"/>
+                    placeholder="••••••••"
+                    style={{
+                        label: "",
+                        input: "w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500" 
+                    }}
+                />
                 <Button type='submit' className='w-full bg-purple-500 hover:bg-purple-600 mt-2 cursor-pointer disabled:opacity-70 disabled:cursor-not-allowed'>
                     {
                     formik.isSubmitting ? (
--- components/motionedCheckbox/MotionChekbox.tsx
+++
@@ -1,9 +0,0 @@
-type TProps = Omit<
-    React.SelectHTMLAttributes<HTMLSelectElement>,
-    "name"
-> & {
-    name: string,
-    label?: string,
-    options: IOptions[],
-    defaultValue?: string,
-}
