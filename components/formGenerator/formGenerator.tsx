3f52a3e6353b97ef7e0d31f60a41271d28c928df
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-04-27 15:10:56 +0300

    create the field provider that will return the jsx of the field to render it inside the form.

---
+++ components/formGenerator/formGenerator.tsx
@@ -0,0 +1,14 @@
+import { IFormField } from '@/@types'
+import React from 'react'
+interface IProps {
+    fields: IFormField[];
+}
+const FormGenerator = (props: IProps) => {
+    return (
+        {
+            
+        }
+    )
+}
+
+export default FormGenerator
