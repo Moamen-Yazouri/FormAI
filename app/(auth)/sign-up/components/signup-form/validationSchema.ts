194e438bdf161f8d8ee8ce2f59fdd31580974c41
Moamen-Yazouri <moaamenalyazouri@gmail.com>
2025-06-19 12:13:18 +0300

    ..

--- app/(auth)/sign-up/components/signup-form/validationSchema.ts
+++ app/(auth)/sign-up/components/signup-form/validationSchema.ts
@@ -16,9 +16,9 @@ export const validationSchema = yup.object({
     password: yup.string()
         .required("Password is required")
         .min(6, "Password must be at least 8 characters long")
-        .matches(/[A-Z]/, "Email must contain at least one uppercase letter")
-        .matches(/[a-z]/, "Email must contain at least one lowercase letter")
-        .matches(/[0-9]/, "Email must contain at least one number"),
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/[0-9]/, "Password must contain at least one number"),
     
     confirmedPassword: yup.string()
         .required("Confirmed password is required")
--- components/scroll-container/scroll-container.tsx
+++ components/scroll-container/scroll-container.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react";
 import { cn } from "@/lib/utils";
-import { useIsMobile } from "@/hooks/use-mobile";
+
 
 interface ScrollableContainerProps extends React.HTMLAttributes<HTMLDivElement> {
     maxHeight?: string
@@ -10,11 +10,11 @@ interface ScrollableContainerProps extends React.HTMLAttributes<HTMLDivElement> 
 
 const ScrollableContainer = React.forwardRef<HTMLDivElement, ScrollableContainerProps>(
     ({ className, children, maxHeight = "70vh", hideScrollbar = false, ...props }, ref) => {
-        const isMobile = useIsMobile();
+        
         return (
         <div
             ref={ref}
-            className={cn("overflow-y-auto", hideScrollbar ? "scrollbar-hide" : "custom-scrollbar pr-2", isMobile && "mt-14",className)}
+            className={cn("overflow-y-auto", hideScrollbar ? "scrollbar-hide" : "custom-scrollbar pr-2", className)}
             style={{ maxHeight }}
             {...props}
         >
@@ -26,4 +26,4 @@ const ScrollableContainer = React.forwardRef<HTMLDivElement, ScrollableContainer
 
 ScrollableContainer.displayName = "ScrollableContainer"
 
-export { ScrollableContainer }
+export { ScrollableContainer };
